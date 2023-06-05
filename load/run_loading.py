32b0a8bbbd45ba046267a408536c4967634a4dbb
Milos Belic <milos.belic@zuehlke.com>
2023-06-05 13:59:31 +0200

    Refactor

--- load/run_loading.py
+++ load/run_loading.py
@@ -1,7 +1,6 @@
 from loader import Loader
 
 if __name__ == "__main__":
-    input_file = 'processed_output.csv'
-    table_name = 'real_estate'
+    input_file = "processed_output.csv"
+    table_name = "real_estate"
     Loader.run(input_file, table_name)
-
